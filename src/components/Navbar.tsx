f9b05bfd8fa581afa476c3cd3cbeb5a4e9777c6c
coding-zero-I <yashsangwan4700@gmail.com>
2025-11-29 21:08:18 +0530

    improved navbar with loading state

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,38 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  useAuth,
+  UserButton,
+} from "@clerk/clerk-react";
 import { ModeToggle } from "./mode-toggle";
+import { Spinner } from "./ui/spinner";
+import { Button } from "./ui/button";
+import { Link } from "react-router-dom";
 
 function Navbar() {
+  const { isLoaded } = useAuth();
   return (
     <header className="w-full h-12 flex items-center px-4">
       <nav className="w-full h-full flex items-center justify-between">
-        <h1 className="italic underline text-3xl">Cognito</h1>
-        <div className="flex gap-4">
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
+        <h1 className="italic underline text-3xl">
+          <Link to="/">Cognito</Link>
+        </h1>
+        <div className="flex gap-4 items-center">
+          {isLoaded ? (
+            <>
+              <SignedOut>
+                <SignInButton>
+                  <Button variant="default">Sign In</Button>
+                </SignInButton>
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </>
+          ) : (
+            <Spinner />
+          )}
           <ModeToggle />
         </div>
       </nav>
