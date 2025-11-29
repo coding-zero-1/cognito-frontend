35da6fb649378d25d54b7072dfe88d80a7ca4bf3
coding-zero-I <yashsangwan4700@gmail.com>
2025-11-29 21:08:42 +0530

    initial setup with loading state

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner";
+import { useAuth } from "@clerk/clerk-react";
+import { Plus, Share } from "lucide-react";
 
 function HomePage() {
+  const { isLoaded } = useAuth();
+  if (!isLoaded) {
+    return (
+      <div className="w-full h-[calc(100vh-12rem)] flex justify-center items-center">
+        <Spinner className="size-8" />
+      </div>
+    );
+  }
   return (
-    <div>HomePage</div>
-  )
+    <main className="w-full h-[calc(100vh-3rem)] px-4 py-2">
+      <div className="h-[7%] w-full flex items-center justify-end gap-4">
+        <Button variant={"outline"}>
+          <Share /> Share Brain
+        </Button>
+        <Button variant={"default"}>
+          <Plus /> Add Card
+        </Button>
+      </div>
+      <div className="w-full min-h-[93%] px-4 py-2">
+      </div>
+    </main>
+  );
 }
 
-export default HomePage
+export default HomePage;
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import { Spinner } from "@/components/ui/spinner";
+import { useAuth } from "@clerk/clerk-react";
 
 function LandingPage() {
+  const { isLoaded } = useAuth();
+  if (!isLoaded) {
+    return (
+      <div className="w-full h-[calc(100vh-12rem)] flex justify-center items-center">
+        <Spinner className="size-8" />
+      </div>
+    );
+  }
   return (
-    <main>
-        <div>LandingPage</div>
+    <main className="w-full h-[calc(100vh-3rem)] px-4 py-2">
+      <div>LandingPage</div>
     </main>
-  )
+  );
 }
 
-export default LandingPage
+export default LandingPage;
