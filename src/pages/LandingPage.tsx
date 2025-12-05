08a38c1b11778b33ae44f54c147fdaaecd518616
coding-zero-I <yashsangwan4700@gmail.com>
2025-12-05 20:17:07 +0530

    intial landing page setup

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,5 +1,6 @@
 import { Spinner } from "@/components/ui/spinner";
 import { useAuth } from "@clerk/clerk-react";
+import { Brain } from "lucide-react";
 
 function LandingPage() {
   const { isLoaded } = useAuth();
@@ -11,8 +12,9 @@ function LandingPage() {
     );
   }
   return (
-    <main className="w-full h-[calc(100vh-3rem)] px-4 py-2">
-      <div>LandingPage</div>
+    <main className="w-full h-[calc(100vh-3rem)] lg:px-4 md:px-2 pt-2 flex flex-col justify-between items-center">
+      <h1 className="md:text-5xl text-4xl flex flex-col md:flex-row items-center justify-center gap-3 mt-5">Your very own<span className="text-[#2B20FF] flex items-center italic underline dark:text-cyan-400">Second Brain <Brain className="inline size-11"/> </span></h1>
+      <img src="/hero-section-image-2.png" alt="" className="h-[80%] md:w-[80%] w-full object-fill rounded-2xl" />
     </main>
   );
 }
