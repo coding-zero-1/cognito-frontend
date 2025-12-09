fa8fdfbc069cf8364bf0cbc724f43111c898a726
coding-zero-I <yashsangwan4700@gmail.com>
2025-12-09 19:32:07 +0530

    fixed twitter embed issue

--- src/lib/formatLink.ts
+++ src/lib/formatLink.ts
@@ -5,7 +5,6 @@ function formatLink(link: string, type: LinkType): string {
 
   switch (type) {
     case "youtube": {
-      // Extract video ID and format as embed link
       const youtubeMatch = link.match(
         /(?:youtu\.be\/|youtube\.com\/(?:watch\?v=|embed\/|v\/|.+\?v=))([^&\n?#]+)/
       );
@@ -17,13 +16,14 @@ function formatLink(link: string, type: LinkType): string {
       }
       break;
     }
-
-    case "twitter":
-      formattedLink = `https://twitter.com/intent/tweet?url=${encodeURIComponent(
-        link
-      )}`;
+    case "twitter": {
+      const arr = link.split("/");
+      console.log(arr);
+      const statusIndex = arr.findIndex((part) => part === "status");
+      const id = arr[statusIndex + 1].split("?")[0];
+      formattedLink = `https://twitter.com/${arr[3]}/status/${id}`;
       break;
-
+    }
     case "image":
     case "text":
     default:
